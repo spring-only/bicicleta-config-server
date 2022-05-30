d11756c29a07722fe63a0facc94ce9b247da5e82
Joseph Toulier Pighi <jtoulier@gmail.com>
2022-05-29 21:36:17 -0500

    Commit inicial

---
+++ src/main/java/com/springonly/bicicletaconfigserver/BicicletaConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.springonly.bicicletaconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BicicletaConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BicicletaConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springonly/bicicletaconfigserver/BicicletaConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springonly.bicicletaconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BicicletaConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
